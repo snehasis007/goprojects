877cde9dfe23a82e75829b4ca5b686e41d04949d
snehasis007 <snehasis007@gmail.com>
2020-05-03 03:48:25 +0530

    first commit

---
+++ src/com/cts/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"com/cts/utils"
+	"fmt"
+)
+
+func main() {
+	utils.Test()
+	fmt.Printf("helloooo, world2\n")
+}
---
+++ src/com/cts/utils/getutils.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"fmt"
+)
+
+var ff string = "fsjhjs"
+
+func init() {
+	fmt.Printf("test")
+}
+func Test() {
+	x := "10"
+	y := "check"
+
+	z := x + y
+	fmt.Printf(ff + z)
+}
